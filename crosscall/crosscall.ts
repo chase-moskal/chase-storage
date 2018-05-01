21f22dd7e1984ec315d9f94ff8344abbd273323e
Chase Moskal <chasemoskal@gmail.com>
2018-04-30 17:00:09 -0700

    refine crosscall concept

--- crosscall/crosscall.ts
+++ crosscall/crosscall.ts
@@ -1,75 +1,4 @@
 
-export interface Callee {
-  [key: string]: (...args: any[]) => Promise<any>
-}
-
-export interface Permission {
-  origin: RegExp
-  allow: string[]
-}
-
-export interface CrossCallHostOptions<gCallee extends Callee = Callee> {
-  callee: gCallee
-  permissions: Permission[]
-}
-
-export type Response = {id: string} & ({data: any} | {error: string})
-
-export interface Request {
-  id: string
-  method: string
-  params: any[]
-}
-
-export class CrossCallHost<gCallee extends Callee = Callee> {
-  private readonly errtag = "cross rpc error -"
-  private readonly callee: gCallee
-  private readonly permissions: Permission[]
-
-  private readonly sendResponse = (payload: Response) => {
-    window.parent.postMessage(JSON.stringify(payload), "*")
-  }
-
-  private readonly handleRequest = async(message: MessageEvent) => {
-    const {callee, permissions} = this
-    const {origin, data} = message
-    const {id, method, params} = <Request>data
-
-    const permission = permissions.find(perm => perm.origin.test(origin))
-    if (!permission)
-      throw new Error(`${this.errtag} no permission for origin "${origin}"`)
-
-    const allowed = permission.allow.find(allowance => allowance === method)
-    if (!allowed)
-      throw new Error(`${this.errtag} permission "${method}" not allowed`)
-
-    try {
-      const result = await callee[method](...params)
-      this.sendResponse({id, data: result})
-    }
-    catch (error) {
-      this.sendResponse({id, error: error.message})
-    }
-  }
-
-  constructor({callee, permissions}: CrossCallHostOptions<gCallee>) {
-    this.callee = callee
-    this.permissions = permissions
-
-    const {handleRequest} = this
-    window.addEventListener("message", handleRequest, false)
-  }
-
-  destructor() {
-    const {handleRequest} = this
-    window.removeEventListener("message", handleRequest)
-  }
-}
-
-export function createCrossCallClient() {
-
-}
-
 /*
 
 const host = new CrossCallHost({
@@ -92,3 +21,87 @@ const client = new CrossCallClient({
 await client.callable.asyncLocalStorage.getItem("lol")
 
 */
+
+import {
+  Callee,
+  Signal,
+  Message,
+  ErrorMessage,
+  Response,
+  HandshakeRequest,
+  CallRequest,
+  Available,
+  HandshakeResponse,
+  CallResponse,
+  Expose,
+  CrossCallHostOptions,
+  CrossCallClientOptions
+} from "./interfaces"
+
+const errtag = "crosscall error -"
+
+export class CrossCallHost<gCallee extends Callee = Callee> {
+  private readonly expose: Expose
+  private messageId = 0
+
+  private sendMessage(data: Message, target: string) {
+    const payload = {...data, id: this.messageId++}
+    window.parent.postMessage(JSON.stringify(payload), target)
+  }
+
+  /**
+   * Handle a request from the crosscall client
+   */
+  private readonly handleRequest = async(message: MessageEvent) => {
+    const {expose} = this
+    const {origin: messageOrigin, data} = message
+    const {id, signal, topic, method, params} = <CallRequest>data
+
+    try {
+
+      // ensure topic is exposed
+      const permission = expose[topic]
+      if (!permission) throw new Error(`${errtag} unknown topic "${topic}" not exposed`)
+
+      // ensure message origin is allowed topic access
+      const {callee, origin: originRegex, methods} = permission
+      const originAllowed = originRegex.test(messageOrigin)
+      if (!originAllowed) throw new Error(`${errtag} origin "${messageOrigin}" not allowed to access "${topic}"`)
+
+      // ensure requested method is allowed
+      const allowed = methods.find(m => m === method)
+      if (!allowed) throw new Error(`${errtag} method "${topic}.${method}" not exposed`)
+
+      // call the method and prepare the response
+      const response = <CallResponse>{
+        signal: Signal.Call,
+        response: id,
+        result: await callee[method](...params)
+      }
+
+      // send the response
+      this.sendMessage(response, messageOrigin)
+    }
+
+    // send back any errors
+    catch (error) {
+      this.sendMessage(<ErrorMessage>{id, error: error.message}, messageOrigin)
+    }
+  }
+
+  constructor({expose}: CrossCallHostOptions) {
+    this.expose = expose
+    const {handleRequest} = this
+    window.addEventListener("message", handleRequest, false)
+  }
+
+  destructor() {
+    const {handleRequest} = this
+    window.removeEventListener("message", handleRequest)
+  }
+}
+
+export class CrossCallClient {
+  readonly callable: Callee
+  constructor({}: CrossCallClientOptions) {}
+}
--- crosscall/interfaces.ts
+++ crosscall/interfaces.ts
@@ -1,4 +1,69 @@
 
+export interface Callee {
+  [method: string]: (...args: any[]) => Promise<any>
+}
+
+export const enum Signal {
+  Handshake,
+  Call
+}
+
+export type Id = string
+
+export interface Message {
+  id?: Id
+  signal: Signal
+}
+
+export interface Response {
+  response: Id
+}
+
+export interface ErrorMessage extends Message, Partial<Response> {
+  error: string
+}
+
+export interface HandshakeRequest extends Message {
+  signal: Signal.Handshake
+}
+
+export interface CallRequest extends Message {
+  signal: Signal.Call
+  topic: string
+  method: string
+  params: any[]
+}
+
+export interface Available {
+  [method: string]: string[]
+}
+
+export interface HandshakeResponse extends Message, Response {
+  signal: Signal.Handshake
+  available: Available
+}
+
+export interface CallResponse extends Message, Response {
+  signal: Signal.Call
+  result: any
+}
+
+export interface Expose {
+  [topic: string]: {
+    callee: Callee
+    origin: RegExp
+    methods: string[]
+  }
+}
+
+export interface CrossCallHostOptions {
+  expose: Expose
+}
+
+export interface CrossCallClientOptions {
+  available: Available
+}
+
 export interface AsyncStorage {
 
   // standard methods
